6ecfa486449f277f385ad5682f23e3be18c1e752
Nurfachri <nurfachridaffa17@gmail.com>
2022-12-29 01:51:27 +0000

    add new SSL

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
-import ssl
 from ktp_ocr_crop import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug = True, host='0.0.0.0', port=7007, ssl_context=('fullchain.pem', 'privkey.pem'))
+    app.run(debug = True, host='0.0.0.0', port=7007, ssl_context=('fullchain.pem','privkey.pem'))
